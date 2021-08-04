b2b76cd6c94b4fda3c36f40fe73f21dfca44cd01
Rostyslav Krukovskyi <rostyslavkrukovskyi@Rostyslavs-MacBook-Pro.local>
2021-08-04 16:41:37 +0300

    Main branch: test change 2 after branch_1 was created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'interview-test-app';
   // test change 1 after initial commit on Main branch
+
+
+  // test change 2 after branch_1 was created on Main bsranch
 }
